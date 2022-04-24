d3075270222bdd4dd3b1fdd32dd22b2a7d074967
Tae Young Ryu <yty0819@gmail.com>
2022-04-25 04:22:12 +0900

    Resolve #8 - Add member vo

---
+++ src/main/java/kr/poi/study/domain/Member.java
@@ -0,0 +1,13 @@
+package kr.poi.study.domain;
+
+import java.time.LocalDateTime;
+
+public class Member {
+
+    private Long memberPk;
+    private String nickName;
+    private String phoneNumber;
+    private LocalDateTime joinDateTime;
+    private LocalDateTime activeDateTime;
+
+}
